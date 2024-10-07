f93b6a38142767bb472d332a8859308520bbd507
Joshua Wilcox <joshwilcox1@icloud.com>
2024-10-07 17:26:36 +0100

    fix

--- Challenge 1/challenge1.py
+++ Challenge 1/challenge1.py
@@ -1,9 +1,13 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 personCode = input("Enter the code of the person you are looking for \n>>")
-codeURL = "https://www.ecs.soton.ac.uk/people/"+personCode
-
+codeURL = "https://www.southampton.ac.uk/people/"+personCode
 request = requests.get(codeURL)
+
 soup = BeautifulSoup(request.text, "html.parser")
-nameElement = soup.find_all("a", {"href": "/people/dem"})
-print(nameElement[0].text)
+nameElement = soup.find_all("h1", {"class": f"heading-m inline-block text-prussianDark"})
+if nameElement == []:
+    print("No Usr Found")
+else:   
+    print(nameElement[0].text)
